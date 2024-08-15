95e4582ab89011e69a0d27f264e8372ab65436a7
skamil123 <skamil123@gmail.com>
2024-08-15 22:20:57 +0200

    1

---
+++ Zadaniedomowedzien6/Zadaniedomowedzien6/Program.cs
@@ -0,0 +1,45 @@
+﻿using Zadaniedomowedzien6;
+
+Employee user1 = new Employee ("Adam", "Adamowicz", "20", 0);
+Employee user2 = new Employee ("Adam", "Kowalski", "25", 0);
+Employee user3 = new Employee ("Adam", "iEwa", "30", 0);
+
+user1.AddScore(9);
+user1.AddScore(9);
+user1.AddScore(9);
+user1.AddScore(9);
+user1.AddScore(9);
+
+user2.AddScore(1);
+user2.AddScore(1);
+user2.AddScore(1);
+user2.AddScore(1);
+user2.AddScore(1);
+
+user3.AddScore(11);
+user3.AddScore(1);
+user3.AddScore(1);
+user3.AddScore(2);
+user3.AddScore(1);
+
+List<Employee> users = new List<Employee>()
+{
+    user1, user2, user3
+};
+
+int maxResult = -1;
+Employee userWithMaxResult = null;
+foreach (var Employee in users )
+{
+if(Employee.Result > maxResult)
+    {
+        maxResult = Employee.Result;
+        userWithMaxResult = Employee; 
+    }
+}
+
+
+Console.WriteLine(userWithMaxResult.Name);
+Console.WriteLine(userWithMaxResult.LastName);
+Console.WriteLine("Wiek " + userWithMaxResult.Age);
+Console.WriteLine(maxResult);
---
+++ Zadaniedomowedzien6/Zadaniedomowedzien6/Zadaniedomowedzien6.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Zadaniedomowedzien6
+{
+    public class Employee
+
+    {
+        private List<int> score = new List<int>();
+        public Employee(string name, string last_name, string age, int score)
+        {
+            this.Name = name;
+            this.LastName = last_name;
+            this.Age = age;
+            this.Score = score;
+
+        }
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public string Age { get; private set; }
+        public int Score { get; private set; }
+        public int Result { get { return this.score.Sum(); } }
+
+        public void AddScore(int score)
+        { this.score.Add(score); }
+   
+
+    }
+}
